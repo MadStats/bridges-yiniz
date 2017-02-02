2697b9180020f5dfb1f6547a69371d2d72419926
yiniz <yinizhang2011@gmail.com>
2017-02-01 21:05:18 -0600

    updated on Feb 1st

--- bridge_excercise.R
+++ bridge_excercise.R
@@ -1,7 +1,15 @@
+library(plyr)
+library(choroplethr)
+library(dplyr)
+library(readr)
+library(data.table)
 getwd()
 #read the bridge file 2015 data
-inData = read.delim("slubkin_992016-20160125163323.txt", sep = ",", header = TRUE)
+# inData = read.delim("slubkin_992016-20160125163323.txt", sep = ",", header = TRUE)
+inData = fread ("https://www.fhwa.dot.gov/bridge/nbi/2016/delimited/CO16.txt")
 View(inData)
 # b_2015 = dt_bridges_2015[, c("STRUCTURE_NUMBER_008","YEAR_BUILT_027","COUNTY_CODE_003", "SUPERSTRUCTURE_COND_059","BRIDGE_IMP_COST_094")]
 # year=mydata[,1]
-outData=data.frame(inData=inData$STRUCTURE_NUMBER_008, year=inData$YEAR_BUILT_027, fips = inData$COUNTY_CODE_003, condition = inData$SUPERSTRUCTURE_COND_059, cost = inData$BRIDGE_IMP_COST_094)
+
+#a simple way to subset data (select variables)
+outData=data.frame(structureNo=inData$STRUCTURE_NUMBER_008, year=inData$YEAR_BUILT_027, fips = inData$COUNTY_CODE_003, condition = inData$SUPERSTRUCTURE_COND_059, cost = inData$BRIDGE_IMP_COST_094)
